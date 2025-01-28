39d7ba0dbf0cf8bd424585162289305977fe7767
Thomas Holmes <thomasholmes@outlook.com>
2025-01-28 21:26:29 +0000

    Ship now gets fires when hit.

--- readme.md
+++ readme.md
@@ -6,4 +6,4 @@ _Better title pending..._
 
 * https://opengameart.org/content/24-x-scifi-cyberpunk-tiles
 * https://murphysdad.itch.io/sci-fi-facility
-
+* https://bdragon1727.itch.io/free-effect-bullet-impact-explosion-32x32
--- scenes/engineering_room.gd
+++ scenes/engineering_room.gd
@@ -10,6 +10,10 @@ extends Node2D
 @onready var engine: ConsumerSubsystem = %engine
 @onready var weapons: ConsumerSubsystem = %weapons
 
+@onready var fire_1_scene = load("res://assets/effects/fire_1.tscn")
+@onready var fire_2_scene = load("res://assets/effects/fire_2.tscn")
+@onready var fire_3_scene = load("res://assets/effects/fire_3.tscn")
+
 var enemy_ships: Array[EnemyShip] = []
 
 
@@ -30,8 +34,22 @@ func _process(delta: float) -> void:
 func ship_damage():
     ship_health -= 1
     camera.shake(0.5)
+    spawn_fire(20)
     if ship_health <= 0:
         Global.game_over("The ship was destroyed be enemy attacks")
+
+func spawn_fire(fires_to_spawn: int):
+    var tile_map: TileMapLayer = %tile_map_floors
+    var floor_tiles = tile_map.get_used_cells_by_id(-1, Vector2i(1, 1))
+    floor_tiles.shuffle()
+    var fire_scenes = [ fire_1_scene, fire_2_scene, fire_3_scene ]
+    var effects_node = %effects
+    for i in range(0, fires_to_spawn):
+        var tile: Vector2i = floor_tiles.pop_front()
+        var position = tile_map.map_to_local(tile)
+        var fire: Node2D = fire_scenes.pick_random().instantiate()
+        effects_node.add_child(fire)
+        fire.position = position
 
 func _on_enemy_ship_state_changed(enemy_ship_state: EnemyShip.EnemyShipState) -> void:
     if enemy_ship_state == EnemyShip.EnemyShipState.PREPARING_ATTACK:
